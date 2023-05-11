9b344ffd4d0fb3f6d17294c5789b7f6865cd7cb3
rahulsharma20231 <132823816+rahulsharma20231@users.noreply.github.com>
2023-05-11 20:04:32 +0530

    Update [...postpath].tsx
    Zubair SHaikh

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -91,18 +91,18 @@ const Post: React.FC<PostProps> = (props) => {
 				<meta property="og:site_name" content={host.split('.')[0]} />
 				<meta property="article:published_time" content={post.dateGmt} />
 				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+				<meta property="og:image" content={post?.featuredImage?.node?.sourceUrl} />
 				<meta
 					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
+					content={post?.featuredImage?.node?.altText || post.title}
 				/>
 				<title>{post.title}</title>
 			</Head>
 			<div className="post-container">
 				<h1>{post.title}</h1>
 				<img
-					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
+					src={post?.featuredImage?.node?.sourceUrl}
+					alt={post?.featuredImage?.node?.altText || post.title}
 				/>
 				<article dangerouslySetInnerHTML={{ __html: post.content }} />
 			</div>
